2f78803046764599084679b79f8fd6be875e4f5d
Emerson Oliveira da Hora <emersondahora@gmail.com>
2020-12-27 11:54:19 -0300

    Order, Instant, ISO 8601 - part 2

---
+++ src/main/java/com/emersondahora/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.emersondahora.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emersondahora.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
